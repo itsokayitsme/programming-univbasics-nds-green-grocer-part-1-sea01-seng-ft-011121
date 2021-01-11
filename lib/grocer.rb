6440dbf571b99af76f938a39513b02e2fdab18e6
Johnny Cao <jcao0928@gmail.com>
2021-01-11 10:00:04 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -10,7 +10,7 @@ def consolidate_cart(cart)
  index = 0
  new_cart = []
  while index < cart.length do
-   item = find_item_by_name_in_collection(cart[index][:item, new_cart)
+   item = find_item_by_name_in_collection(cart[index][:item], new_cart)
    if item
      new_cart_index = 0
      while new_cart_index < new_cart.length do
